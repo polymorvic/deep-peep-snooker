3c96e0176bcd4817dc8ff8e1d5460aa783061892
polymorvic <polymorphic.bite@gmail.com>
2025-10-28 13:56:12 +0100

    add file to set global project options

---
+++ src/config.py
@@ -0,0 +1,10 @@
+"""
+Global configuration for the project.
+
+This module sets up global settings for the entire project, including
+matplotlib display preferences and other project-wide configurations.
+"""
+
+import matplotlib.pyplot as plt
+
+plt.rcParams['image.cmap'] = 'gray'
