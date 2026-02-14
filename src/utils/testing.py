550acfeb7c68d8852a559950879a06aa52890a2d
polymorvic <polymorphic.bite@gmail.com>
2026-02-15 00:06:02 +0100

    add test bottom line function

--- src/utils/testing.py
+++ src/utils/testing.py
@@ -2,9 +2,17 @@ from datetime import datetime
 from typing import Literal
 from pathlib import Path
 from enum import StrEnum
+
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+from .annotations import transform_annotation, PolygonAnnotationData
+from .common import NumpyImage
+from .func import read_image_as_numpyimage
+from .lines import Line
+from .playfield_finder import PlayfieldFinder
 
 
 TestType = Literal["bottom", "top", "left", "right", "iou"]
@@ -78,6 +86,15 @@ class TestType(StrEnum):
         if self is TestType.IOU:
             return "iou"
         return f"internal-{self.value}-cushion"
+    
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, str):
+            value = value.lower()
+            for member in cls:
+                if member.value == value:
+                    return member
+        return None
 
 
 def build_output_dir(parent_dir: str | Path, test_type: TestType) -> Path:
@@ -85,3 +102,26 @@ def build_output_dir(parent_dir: str | Path, test_type: TestType) -> Path:
     out_dir = Path(parent_dir) / test_type.subdir / ts
     out_dir.mkdir(parents=True, exist_ok=True)
     return out_dir
+
+
+def test_bottom(pic_filepath: Path, 
+                polygon_ann: list[PolygonAnnotationData],
+                test_out_dir: Path
+                ) -> tuple[Line, int]:
+
+    pic_name = pic_filepath.name
+    pic = read_image_as_numpyimage(pic_filepath, "rgb")
+    finder = PlayfieldFinder(pic)
+
+    bottom = finder.find_bottom_internal_cushion()
+
+    data = polygon_ann.filter_by_image(pic_name)
+    pts_gt = np.asarray(transform_annotation(pic, data.points))
+    y_ref = int(np.median(np.sort(pts_gt[:, 1])[-2:]))
+
+    p1, p2 = bottom.limit_to_img(pic)
+    img = cv2.cvtColor(pic, cv2.COLOR_RGB2BGR)
+    cv2.line(img, p1, p2, (255, 0, 0), 1)
+    cv2.imwrite(str(test_out_dir / f"test_{pic_name}"), img)
+
+    return bottom, y_ref
