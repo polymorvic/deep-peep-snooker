d5af857d3a384381206967313f9494fa98a732f4
polymorvic <polymorphic.bite@gmail.com>
2026-03-30 07:37:30 +0200

    rename ImagePlayfieldAnnotation

--- src/utils/schemas.py
+++ src/utils/schemas.py
@@ -30,31 +30,6 @@ class ImageBallAnnotation(ImageAnnotation):
     balls: list[Ball]
 
 
-class ImagePolygonAnnotation(ImageAnnotation):
+class ImagePlayfieldAnnotation(ImageAnnotation):
     points: list[list[float]]
 
-
-
-
-
-
-class PolygonAnnotationData(BaseModel):
-    """Polygon annotation data model.
-    
-    Represents a single polygon annotation with its associated image metadata.
-    
-    Attributes:
-        points: List of [x, y] coordinate pairs defining the polygon vertices.
-        image: Image metadata containing filename and dimensions.
-    """
-    points: list[list[float]]
-    image: ImageMetaData
-
-
-class PolygonAnnotationList(BaseModel):
-    """Container for a list of polygon annotations.
-    
-    Attributes:
-        annotations: List of polygon annotations.
-    """
-    annotations: list[PolygonAnnotationData]
