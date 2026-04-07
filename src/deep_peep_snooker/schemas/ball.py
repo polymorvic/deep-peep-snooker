050c051decd7b7a34e4805c152ea7c124cc3b805
polymorvic <polymorphic.bite@gmail.com>
2026-04-07 12:50:32 +0200

    add BallRanges datamodel

--- src/deep_peep_snooker/schemas/ball.py
+++ src/deep_peep_snooker/schemas/ball.py
@@ -1,6 +1,5 @@
 import numpy as np
 from enum import StrEnum
-from typing import Mapping
 
 from deep_peep_snooker.schemas.base import SnookerModel
 
@@ -21,9 +20,20 @@ class BallHSVRange(SnookerModel):
     upper_bound: np.ndarray
 
 
+class BallRanges(SnookerModel):
+    blue: BallHSVRange
+    yellow: BallHSVRange
+    green: BallHSVRange
+    brown: BallHSVRange
+    pink: BallHSVRange
+    red: BallHSVRange
+    cue: BallHSVRange
+    black: BallHSVRange
+
+
 class BallsConfig(SnookerModel):
     radius_mm: float
-    ranges: Mapping[StrEnum, BallHSVRange]
+    ranges: BallRanges
 
-    def range_for(self, color: StrEnum) -> BallHSVRange:
-        return self.ranges[color]
+    def range_for(self, color: BallColor) -> BallHSVRange:
+        return getattr(self.ranges, color.value)
