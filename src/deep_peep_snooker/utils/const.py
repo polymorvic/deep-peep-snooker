2232472d67e0cf3280dbcaf1ea3cd6af994c5575
polymorvic <polymorphic.bite@gmail.com>
2026-04-07 12:45:48 +0200

    fix balls config instance in const

--- src/deep_peep_snooker/utils/const.py
+++ src/deep_peep_snooker/utils/const.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from deep_peep_snooker.schemas.ball import BallHSVRange, BallsConfig, BallColor
+from deep_peep_snooker.schemas.ball import BallHSVRange, BallsConfig, BallColor, BallRanges
 
 
 GREEN_LOWER_BOUND = np.array([25, 40, 40])
@@ -11,39 +11,39 @@ GREEN_UPPER_BOUND = np.array([95, 255, 255])
 
 BALLS = BallsConfig(
     radius_mm=52.5,
-    ranges={
-        BallColor.BLUE: BallHSVRange(
+    ranges=BallRanges(
+        blue=BallHSVRange(
             lower_bound=np.array([100, 100, 100]),
             upper_bound=np.array([140, 255, 255]),
         ),
-        BallColor.YELLOW: BallHSVRange(
+        yellow=BallHSVRange(
             lower_bound=np.array([20, 100, 100]),
             upper_bound=np.array([30, 255, 255]),
         ),
-        BallColor.GREEN: BallHSVRange(
+        green=BallHSVRange(
             lower_bound=np.array([30, 100, 100]),
             upper_bound=np.array([70, 255, 255]),
         ),
-        BallColor.BROWN: BallHSVRange(
+        brown=BallHSVRange(
             lower_bound=np.array([10, 100, 100]),
             upper_bound=np.array([20, 255, 255]),
         ),
-        BallColor.BLACK: BallHSVRange(
+        black=BallHSVRange(
             lower_bound=np.array([0, 100, 100]),
             upper_bound=np.array([10, 255, 255]),
         ),
-        BallColor.CUE: BallHSVRange(
+        cue=BallHSVRange(
             lower_bound=np.array([0, 100, 100]),
             upper_bound=np.array([10, 255, 255]),
         ),
-        BallColor.RED: BallHSVRange(
+        red=BallHSVRange(
             lower_bound=np.array([0, 100, 100]),
             upper_bound=np.array([10, 255, 255]),
         ),
-        BallColor.PINK: BallHSVRange(
+        pink=BallHSVRange(
             lower_bound=np.array([150, 100, 100]),
             upper_bound=np.array([170, 255, 255]),
         ),
-    },
+    ),
 )
 
